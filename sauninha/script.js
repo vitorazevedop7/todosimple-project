90f4a8d64a4ff2d32f6c84de108c17dac04517b4
vitorazevedop7 <vitorazevedo.p7@gmail.com>
2025-02-25 13:24:51 -0300

    Teste 1

---
+++ sauninha/script.js
@@ -0,0 +1,7 @@
+document.getElementById('yes').addEventListener('click', function() {
+    document.getElementById('response').innerHTML = 'CONFIRMADO! &#128077;'; // Joinha emoji
+});
+
+document.getElementById('no').addEventListener('click', function() {
+    document.getElementById('response').innerHTML = 'VOCE E GAY';
+});
