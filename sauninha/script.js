207e8d39624fe676df6af72f348c8545e9c16657
vitorazevedop7 <vitorazevedo.p7@gmail.com>
2025-04-25 07:49:33 -0300

    apagado

--- sauninha/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('yes').addEventListener('click', function() {
-    document.getElementById('response').innerHTML = 'CONFIRMADO! &#128077;'; // Joinha emoji
-});
-
-document.getElementById('no').addEventListener('click', function() {
-    document.getElementById('response').innerHTML = 'VOCE E GAY';
-});
